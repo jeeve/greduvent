229a095717ae6e6c714a32574a071618e9022196
jeeve <jvjulien@free.fr>
2019-03-06 00:07:16 +0100

    rives

--- musique/musique.php
+++ musique/musique.php
@@ -137,6 +137,8 @@
                    <div class="containerPlayer">
                         <div style="display: none;">
                            <ul id="playListContainer">
+							  <li data-src="songs/les-rives-de-l-inconscient.mp3"><a href="#">Les rives de l'inconscient</a>
+                              </li>							   						   
 							  <li data-src="songs/emergence.mp3"><a href="#">Emergence</a>
                               </li>							   
 							  <li data-src="songs/entropie.mp3"><a href="#">Entropie</a>
