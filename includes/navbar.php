049a07c8fd07afd5edfa44002d10532656522021
jeeve <jvjulien@free.fr>
2019-11-20 12:39:33 +0100

    musique

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,7 +13,7 @@
                      <span class="icon-bar"></span></span>
                </button>
                <a class="navbar-brand" href="/index.php" style="text-decoration:none">au gré du vent 1.0</a>
-               <a onclick="return !window.open(this.href, 'pop', 'width=315,height=295,left=0,top=0');" href="/musique/musique.php"><img alt="Ecouter Cuisine Spectrale" title="Ecouter Cuisine Spectrale" src="/images/enceinte.gif" class="navbar-brand"/></a>
+               <a onclick="return !window.open(this.href, 'pop', 'width=320,height=400,left=0,top=0');" href="/musique/musique.php"><img alt="Ecouter Cuisine Spectrale" title="Ecouter Cuisine Spectrale" src="/images/enceinte.gif" class="navbar-brand"/></a>
                <a href="/commentaires/commentaires.php"><img alt="Livre d'or" title="Livre d'or" src="/images/livre.png" class="navbar-brand hidden-xs"/></a>
                <a rel="alternate" type="application/rss+xml" href="/news.xml" target="_blank"><img alt="Flux RSS" title="Flux RSS" src="/images/logo_rss.png" class="navbar-brand hidden-xs"/></a>
                <a href="http://www.twitter.com/jeeve" target="_blank"><img alt="@jeeve" title="@jeeve" src="/images/logo_twitter.png" class="navbar-brand hidden-xs"/></a>
--- musique/musique.php
+++ musique/musique.php
@@ -11,8 +11,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
-      <META NAME="Description" CONTENT="Au travers de textes et de photos perso, ces pages sont une invitation au rêve. Sensible aux visages de la nature, je propose une petite balade en sa compagnie."/>
-      <META NAME="Keywords" CONTENT="balade,poésie,windsurf,planche,loire,jean-valéry,vent,nature,photo,texte,kitesurf"/>
+      <META NAME="Description" CONTENT="Groupe musical Cuisine Spectrale."/>
+      <META NAME="Keywords" CONTENT="musique,electro"/>
       <META NAME="Author" CONTENT="Jean-Valéry"/>
       <link rel="shortcut icon" href="../images/favicon_16_16.ico" />
       <link rel="icon" href="../images/favicon_32_32.png" sizes="32x32" />
@@ -131,112 +131,9 @@
 
          <div style="position: absolute">
 
+<div style="position:fixed;">
                   <canvas id='canvas' height="150px"></canvas>
 
-			
-                   <div class="containerPlayer">
-                        <div style="display: none;">
-                           <ul id="playListContainer">
-							  <li data-src="songs/un-sourire.mp3"><a href="#">Un sourire</a>
-                              </li>							   
-							  <li data-src="songs/adada-bar.mp3"><a href="#">Adada bar</a>
-                              </li>							   						   						   
-							  <li data-src="songs/horreur-boreale.mp3"><a href="#">Horreur boréale</a>
-                              </li>								  
-							  <li data-src="songs/horreur-boreale.mp3"><a href="#">Horreur boréale</a>
-                              </li>							   						   						   
-							  <li data-src="songs/les-rives-de-l-inconscient.mp3"><a href="#">Les rives de l'inconscient</a>
-                              </li>							   						   
-							  <li data-src="songs/emergence.mp3"><a href="#">Emergence</a>
-                              </li>							   
-							  <li data-src="songs/entropie.mp3"><a href="#">Entropie</a>
-                              </li>							   
-							  <li data-src="songs/max-et-lulu.mp3"><a href="#">Max et Lulu</a>
-                              </li>							   
-							  <li data-src="songs/epicentre.mp3"><a href="#">Epicentre</a>
-                              </li>							   
-							  <li data-src="songs/alteration.mp3"><a href="#">Altération</a>
-                              </li>							   
-							  <li data-src="songs/peripherique-fluide.mp3"><a href="#">Périphérique fluide</a>
-                              </li>						   
-							  <li data-src="songs/albert-code.mp3"><a href="#">Albert code</a>
-                              </li>							   
-							  <li data-src="songs/le-funambule.mp3"><a href="#">Le funambule</a>
-                              </li>							   
-							  <li data-src="songs/google-chante-d-ormesson.mp3"><a href="#">Google chante d'Ormesson</a>
-                              </li>							   
-							  <li data-src="songs/fusion.mp3"><a href="#">Fusion</a>
-                              </li>							   
-							  <li data-src="songs/confiture-apologie.mp3"><a href="#">Confiture apologie</a>
-                              </li>							   							   
-							  <li data-src="songs/deux-valises.mp3"><a href="#">Deux valises</a>
-                              </li>	
-                              <li data-src="songs/entrevue-spatiale.mp3"><a href="#">Entrevue spatiale</a>
-                              </li>
-                              <li data-src="songs/percutrip.mp3"><a href="#">Percutrip</a>
-                              </li>
-                              <li data-src="songs/vol-au-dessus-d-un-champ-de-salicornes.mp3"><a href="#">Vol au-dessus d'un champ de salicornes</a>
-                              </li>
-                              <li data-src="songs/secteur-303.mp3"><a href="#">Secteur 303</a>
-                              </li>
-                              <li data-src="songs/immersion.mp3"><a href="#">Immersion</a>
-                              </li>
-                              <li data-src="songs/un-soir-sur-le-rocher.mp3"><a href="#">Un Soir sur le Rocher</a>
-                              </li>
-                              <li data-src="songs/le-chant-des-songes.mp3"><a href="#">Le Chant des Songes</a>
-                              </li>
-                              <li data-src="songs/ballet-celeste.mp3"><a href="#">Ballet Celeste</a>
-                              </li>
-                              <li data-src="songs/aurore.mp3"><a href="#">Aurore</a>
-                              </li>
-                              <li data-src="songs/osez-jose.mp3"><a href="#">Osez Jose</a>
-                              </li>
-                              <li data-src="songs/rillette-apotheose.mp3"><a href="#">Rillette Apotheose</a>
-                              </li>
-                              <li data-src="songs/danse-des-sables.mp3"><a href="#">Danse des Sables</a>
-                              </li>
-                              <li data-src="songs/tourbillons.mp3"><a href="#">Tourbillons</a>
-                              </li>
-                              <li data-src="songs/tentation.mp3"><a href="#">Tentation</a>
-                              </li>
-                              <li data-src="songs/reverie.mp3"><a href="#">Reverie</a>
-                              </li>
-                              <li data-src="songs/malakoff-dance.mp3"><a href="#">Malakoff Dance</a>
-                              </li>
-                              <li data-src="songs/les-yeux-de-gaston.mp3"><a href="#">Les Yeux de Gaston</a>
-                              </li>
-                              <li data-src="songs/hiver.mp3"><a href="#">Hiver</a>
-                              </li>
-                              <li data-src="songs/halebop.mp3"><a href="#">Halebop</a>
-                              </li>
-                              <li data-src="songs/balade-a-marre-basse.mp3"><a href="#">Balade a Maree Basse</a>
-                              </li>
-                              <li data-src="songs/avant-qu-il-ne-soit-trop-tard.mp3"><a href="#">Avant qu'il ne Soit Trop Tard</a>
-                              </li>
-                              <li data-src="songs/alors-michel.mp3"><a href="#">Alors Michel</a>
-                              </li>
-                              <li data-src="songs/haas-l-eau-wind.mp3"><a href="#">Haas l Eau Wind</a>
-                              </li>
-                              <li data-src="songs/errance.mp3"><a href="#">Errance</a>
-                              </li>
-                              <li data-src="songs/expedition-montparnasse.mp3"><a href="#">Expedition Montparnasse</a>
-                              </li>
-                              <li data-src="songs/dernier-signal.mp3"><a href="#">Dernier Signal</a>
-                              </li>
-                              <li data-src="songs/onde-nocturne.mp3"><a href="#">Onde Nocturne</a>
-                              </li>
-                              <li data-src="songs/aquanoide.mp3"><a href="#">Aquanoide</a>
-                              </li>
-                              <li data-src="songs/bush-attack.mp3"><a href="#">Bush Attack</a>
-                              </li>
-                              <li data-src="songs/evasion.mp3"><a href="#">Evasion</a>
-                              </li>
-                              <li data-src="songs/uranus-experience.mp3"><a href="#">Uranus Experience</a>
-                              </li>
-                              <li data-src="songs/dark-vadormir.mp3"><a href="#">Dark Vadormir</a>
-                              </li>
-                            </ul>
-                        </div>
                         <div id="playerContainer">
                            <div id="controlContainer">
                               <div class="audioDetails">
@@ -253,10 +150,7 @@
                               <input class="bar" data-attr="rangeVolume" type="range" min="0" max="1" step="0.1" value="0.7" />
                            </div>
                         </div>
-                     </div>
- 
-
-			
+						
    <button type="button" id="BtnShuffle" class="btn btn-default btn-lg active" data-attr="shuffled" style="margin-left:12px">
 		<span class="glyphicon glyphicon-random" aria-hidden="true"></span>
    </button>
@@ -275,6 +169,65 @@
    <button type="button" id="BtnRepeat" class="btn btn-default btn-lg" data-attr="repeatSong">
 		<span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>
    </button>
+			</div>
+                   <div class="containerPlayer">
+                        <div>
+                           <ul id="playListContainer">
+							  <li data-src="songs/un-sourire.mp3"><a href="#">Un sourire</a></li>							   
+							  <li data-src="songs/adada-bar.mp3"><a href="#">Adada bar</a></li>							   						   						   
+                              <li data-src="songs/horreur-boreale.mp3"><a href="#">Horreur boréale</a></li>							   						   						   
+							  <li data-src="songs/les-rives-de-l-inconscient.mp3"><a href="#">Les rives de l'inconscient</a></li>							   						   
+							  <li data-src="songs/emergence.mp3"><a href="#">Emergence</a></li>							   
+							  <li data-src="songs/entropie.mp3"><a href="#">Entropie</a></li>							   
+							  <li data-src="songs/max-et-lulu.mp3"><a href="#">Max et Lulu</a></li>							   
+							  <li data-src="songs/epicentre.mp3"><a href="#">Epicentre</a></li>							   
+							  <li data-src="songs/alteration.mp3"><a href="#">Altération</a></li>							   
+							  <li data-src="songs/peripherique-fluide.mp3"><a href="#">Périphérique fluide</a></li>						   
+							  <li data-src="songs/albert-code.mp3"><a href="#">Albert code</a></li>							   
+							  <li data-src="songs/le-funambule.mp3"><a href="#">Le funambule</a></li>							   
+							  <li data-src="songs/google-chante-d-ormesson.mp3"><a href="#">Google chante d'Ormesson</a></li>							   
+							  <li data-src="songs/fusion.mp3"><a href="#">Fusion</a></li>							   
+							  <li data-src="songs/confiture-apologie.mp3"><a href="#">Confiture apologie</a></li>							   							   
+							  <li data-src="songs/deux-valises.mp3"><a href="#">Deux valises</a></li>	
+                              <li data-src="songs/entrevue-spatiale.mp3"><a href="#">Entrevue spatiale</a></li>
+                              <li data-src="songs/percutrip.mp3"><a href="#">Percutrip</a></li>
+                              <li data-src="songs/vol-au-dessus-d-un-champ-de-salicornes.mp3"><a href="#">Vol au-dessus d'un champ de salicornes</a></li>
+                              <li data-src="songs/secteur-303.mp3"><a href="#">Secteur 303</a></li>
+                              <li data-src="songs/immersion.mp3"><a href="#">Immersion</a></li>
+                              <li data-src="songs/un-soir-sur-le-rocher.mp3"><a href="#">Un Soir sur le Rocher</a></li>
+                              <li data-src="songs/le-chant-des-songes.mp3"><a href="#">Le Chant des Songes</a></li>
+                              <li data-src="songs/ballet-celeste.mp3"><a href="#">Ballet Celeste</a></li>
+                              <li data-src="songs/aurore.mp3"><a href="#">Aurore</a></li>
+                              <li data-src="songs/osez-jose.mp3"><a href="#">Osez Jose</a></li>
+                              <li data-src="songs/rillette-apotheose.mp3"><a href="#">Rillette Apotheose</a></li>
+                              <li data-src="songs/danse-des-sables.mp3"><a href="#">Danse des Sables</a></li>
+                              <li data-src="songs/tourbillons.mp3"><a href="#">Tourbillons</a></li>
+                              <li data-src="songs/tentation.mp3"><a href="#">Tentation</a></li>
+                              <li data-src="songs/reverie.mp3"><a href="#">Rêverie</a></li>
+							  <li data-src="songs/malakoff-dance.mp3"><a href="#">Malakoff Dance</a></li>
+                              <li data-src="songs/les-yeux-de-gaston.mp3"><a href="#">Les Yeux de Gaston</a></li>
+                              <li data-src="songs/hiver.mp3"><a href="#">Hiver</a></li>
+                              <li data-src="songs/halebop.mp3"><a href="#">Halebop</a></li>
+                              <li data-src="songs/balade-a-marre-basse.mp3"><a href="#">Balade a Maree Basse</a></li>
+                              <li data-src="songs/avant-qu-il-ne-soit-trop-tard.mp3"><a href="#">Avant qu'il ne Soit Trop Tard</a></li>
+                              <li data-src="songs/alors-michel.mp3"><a href="#">Alors Michel</a></li>
+                              <li data-src="songs/haas-l-eau-wind.mp3"><a href="#">Haas l Eau Wind</a></li>
+                              <li data-src="songs/errance.mp3"><a href="#">Errance</a></li>
+                              <li data-src="songs/expedition-montparnasse.mp3"><a href="#">Expedition Montparnasse</a></li>
+                              <li data-src="songs/dernier-signal.mp3"><a href="#">Dernier Signal</a></li>
+                              <li data-src="songs/onde-nocturne.mp3"><a href="#">Onde Nocturne</a></li>
+                              <li data-src="songs/aquanoide.mp3"><a href="#">Aquanoide</a></li>
+                              <li data-src="songs/bush-attack.mp3"><a href="#">Bush Attack</a></li>
+                              <li data-src="songs/evasion.mp3"><a href="#">Evasion</a></li>
+                              <li data-src="songs/uranus-experience.mp3"><a href="#">Uranus Experience</a></li>
+                              <li data-src="songs/dark-vadormir.mp3"><a href="#">Dark Vadormir</a></li>
+                            </ul>
+                        </div>
+
+                     </div>
+ 
+
+		
    
 	</div>
          <div style="position: absolute; left: 250px">	
