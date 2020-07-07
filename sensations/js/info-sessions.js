4e6f99bd53342936903b51a0e7ab29e38a8730ee
jeeve <jyvaisj@gmail.com>
2020-07-07 15:44:37 +0200

    vmax

--- sensations/js/info-sessions.js
+++ sensations/js/info-sessions.js
@@ -31,10 +31,7 @@ function getInfoSessions(spot) {
 					voile = ligne.gsx$voile.$t;
 					aileron = ligne.gsx$aileron.$t;
 					distance = ligne.gsx$distancekm.$t;
-					vmax = ligne.gsx$vmaxpolarkmh;// ligne.gsx$vmaxk72noeuds.$t;
-					if (vmax != '') {
-						vmax =  parseFloat(vmax) / 1.852; // passage en noeuds
-					}
+					vmax = ligne.gsx$vmaxk72noeuds.$t;
 					v100m = ligne.gsx$v100mk72.$t;
 					
 					ancre = laDate.replace(new RegExp('/', 'g'), '-');
