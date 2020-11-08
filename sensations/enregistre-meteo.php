2880c9672ccd0159684d4befff613e02bc2b01bf
jeeve <jyvaisj@gmail.com>
2020-11-08 15:38:40 +0100

    time

--- sensations/enregistre-meteo.php
+++ sensations/enregistre-meteo.php
@@ -1,39 +1,46 @@
 
 <!-- https://console.cron-job.org/jobs -->
+
 	  <?php
+	//  header("Access-Control-Allow-Origin: *");
+	 ob_start();
+//	  set_time_limit(0); 
+//	  ini_set('default_socket_timeout', 900); // 900 Seconds = 15 Minutes
 	  
-	  ob_start();
-	  
-		$station = 'I27LERY4';
+		$station = 'louviers';
 		if ($_GET['station'] != '') {
 				$station = $_GET['station'];
 		}	
 		
-		$credit = '1';
-		if ($_GET['credit'] != '') {
-				$credit = $_GET['credit'];
-		}	
-	
-	
+		$ctx = stream_context_create(array('http'=>
+			array(
+				'timeout' => 1200,  //1200 Seconds is 20 Minutes
+			)
+		));
 		
+		$contents = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=' . $station . ',fr&appid=a132b29f173b49547663b30b40006e6f', false, $ctx);
+		
+		$obj = json_decode($contents);
+		
+
+		
+	$temperature = $obj->main->temp;
+	$vitesse = $obj->wind->speed;
+	$direction = $obj->wind->deg;
+		
+		if ($temperature != "") {
+			$temperature = $temperature - 273.15;
+		}
+			
+		
+		if ($vitesse != "") {
+			$vitesse = $vitesse * 1.944;
+		}
+		
+	//	file_get_contents("https://script.google.com/macros/s/AKfycbwNiYJL49ynuyGNkqW0K-P3yCBm74lH3V2INQYDl_9Giqe557A/exec?station=%27" . $station . "%27&temperature=%27" . $temperature . "%27&vitesse=%27" . $vitesse . "%27&orientation=%27" . $direction . "%27");
 		
 		?>
 		
-<html>
-<body>
 
 
-				
-	  <script src="/js/jquery.min.js"></script>
-	  <script type="text/javascript" src="js/enregistre-meteo.js"></script>	  
-      <script> 
-		station = "<?php echo $station; ?>"
-		enregistreMeteo(station);		 
-      </script>
-</body>				
-</html>
-<?php
 
-ob_end_flush();
-
-?>
