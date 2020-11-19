a1ab7251cd93ff14f8cec7784b93e0bb096f05ea
jeeve <jyvaisj@gmail.com>
2020-11-19 08:38:48 +0100

    new hour

--- sensations/enregistre-meteo.php
+++ sensations/enregistre-meteo.php
@@ -16,8 +16,8 @@
 		return;
 	}
 	
-	$difference = strtotime( $current_time ) - strtotime( "20:00:00" );
-	if ($difference > 0) {
+	$difference = strtotime( $current_time ) - strtotime( "21:05:00" );
+	if ($difference >= 0) {
 		return;
 	}	
 	
