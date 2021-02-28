0b8c0731c42b5c1b03c0bf38e700b351c8cb2092
jeeve <jyvaisj@gmail.com>
2021-02-28 16:46:34 +0100

    it

--- sensations/foret-orient.php
+++ sensations/foret-orient.php
@@ -307,7 +307,7 @@
 			var lat1 = position.coords.latitude;
 			var lon1 = position.coords.longitude;	
 			var itineraireSrc = 'https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d353971.9888214055!2d2.2661361853305393!3d48.91901435220709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d' + lat1 + '!2d' + lon1 + '!4m5!';	
-			jQuery('#map-foret-orient').attr('src', itineraireSrc + '1s0x47ec2375d1e1f51d%3A0x40a5fb99a3f4060!2s10140%20Mesnil-Saint-P%C3%A8re!3m2!1d48.248976899999995!2d4.333515!5e0!3m2!1sfr!2sfr!4v1614520308696!5m2!1sfr!2sfr');
+			jQuery('#map-foret-orient').attr('src', itineraireSrc + '1s0x47ec2375d1e1f51d%3A0x40a5fb99a3f4060!2s10140%20Mesnil-Saint-P%C3%A8re!3m2!1d48.248976899999995!2d4.333515!5e0!3m2!1sfr!2sfr!4v1614520308696');
 	}
 		
 		
