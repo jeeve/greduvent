7f20b1b0183b8e7ab02b41e1add83a39c58176ab
jeeve <jyvaisj@gmail.com>
2021-03-17 13:52:58 +0100

    diapo

--- sensations/windsurf-kitesurf.php
+++ sensations/windsurf-kitesurf.php
@@ -169,15 +169,46 @@
 </div>
 <br>	
 
+<br>
+<div class="row">
+<div class="col-xs-12 col-sm-1 fond"></div>
+<div class="col-xs-12 col-sm-10 center-block fond-table encadrement-table">
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active"> <img src="images/diapos/tintin-foil.jpg" alt="Jablines - 17 février 2021 - photo Tintin" title="Jablines - 17 février 2021 - photo Tintin"></div>
+    <div class="item"> <img src="images/diapos/tintin-drone.jpg" alt="Moisson Lavacourt - 12 juillet 2020" title="Moisson Lavacourt - 12 juillet 2020"></div>
+	<div class="item"> <img src="images/diapos/haut.jpg" alt="Vaires sur Marne - 14 mars 2021" title="Vaires sur Marne - 14 mars 2021"></div>
+    <div class="item"> <img src="images/diapos/david-foil.jpg" alt="Moisson Lavacourt - 4 juillet 2020" title="Moisson Lavacourt - 4 juillet 2020"></div>
+	<div class="item"> <img src="images/diapos/tintin-drone2.jpg" alt="Moisson Lavacourt - 25 juillet 2020" title="Moisson Lavacourt - 25 juillet 2020"></div>
+	<div class="item"> <img src="images/diapos/mimi-foil.jpg" alt="Moisson Lavacourt - 12 juillet 2020 - photo Mireille M." title="Moisson Lavacourt - 12 juillet 2020 - photo Mireille M."></div>
+	<div class="item"> <img src="images/diapos/david-haas-foil.jpg" alt="Les Haas - 3 août 2020 - photo David M." title="Les Haas - 3 août 2020 - photo David M."></div>
+	<div class="item"> <img src="images/diapos/v8-foil.jpg" alt="Jablines - 20 février 2021 - photo Olive91" title="Jablines - 20 février 2021 - photo Olive91"></div>
+ </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>	
+
+</div></div>
+<br>
 
 
+<!--
 				  <p align="center">
 				  <a href="https://windsurf-sessions.eg2.fr/mws_new/infos_session.php?target=id_rider&id_rider=3185&id_session=260968" target="_blank">
 				  <img alt="Jablines - 20 février 2021 - prise de vue Olive91" title="Jablines - 20 février 2021 - prise de vue Olive91" src="images/v8-foil.jpg" class="img-responsive ombre-image" />
 				  </a>
 				  </p>
 				  <p class="legende"><a href="https://windsurf-sessions.eg2.fr/mws_new/infos_session.php?target=id_rider&id_rider=3185&id_session=260968" target="_blank">En vol à Jablines</a></p>
-
+-->
 				  <a name="ca-zef-au-crotoy"></a>
 				  <p align="center">
 				  <a href="http://windsurf-sessions.eg2.fr/infos_session.php?id_session=166956" target="_blank">
@@ -263,6 +294,7 @@
       </div>
       <!--/.page-container-->
 	  <?php include("../includes/footer.php"); ?>	
+	  <?php include("../includes/swipe-diapo.php"); ?>
       <script src="https://www.google.com/jsapi"></script> 	  
       <script> 
 	  var vitesses1 = [];
