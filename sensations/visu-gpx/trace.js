61f44c3732b30abf8ebcc61d4c1fb343db790610
jeeve <jyvaisj@gmail.com>
2021-07-15 14:59:44 +0200

    mouse

--- sensations/visu-gpx/trace.js
+++ sensations/visu-gpx/trace.js
@@ -1,23 +1,22 @@
-function calcCrow(lat1, lon1, lat2, lon2) 
-    {
-      var R = 6371; // km
-      var dLat = toRad(lat2-lat1);
-      var dLon = toRad(lon2-lon1);
-      var lat1 = toRad(lat1);
-      var lat2 = toRad(lat2);
+function calcCrow(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
 
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c;
-      return d;
-    }
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
 
-    // Converts numeric degrees to radians
-    function toRad(Value) 
-    {
-        return Value * Math.PI / 180;
-    }
+// Converts numeric degrees to radians
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
+}
 
 var times = [];
 var xy = [];
@@ -27,78 +26,78 @@ var trace;
 var vmax;
 var chartxy = [];
 var marker;
-var chart, line, down;
+var chart, rect, down;
 
 function litGPX(url, times, xy, v, ready) {
   $.ajax({
-    type: "GET",
-    url: url,
-    dataType: "xml",
-    success: function(xml) { 
-            var i;
-            var t1, t2, dd, dt;
-      
-            $(xml).find('trkpt').each(function() {
-              if ($(this).find('time').length == 1) {
-                time = $(this).find('time').text();
-                lat = parseFloat($(this).attr('lat'));
-                lon = parseFloat($(this).attr('lon'));
-                
-                times.push(time);
-                var coord = [];
-                coord[0] = lat;
-                coord[1] = lon;
-                xy.push(coord);
-              }             
-          });
-      
-          var d = 0;
-          chartxy = [];
-          chartxy.push(['Temps', 'Vitesse']);
-          for (i = 0; i < times.length; i++) {
-              if (i == 0) {
-                v.push(0.0);
-              }
-              else {
-                dd = calcCrow(xy[i][0], xy[i][1], xy[i-1][0], xy[i-1][1])*1000;
-                t1 = new Date(times[i-1]);
-                t2 = new Date(times[i]);
-                dt = (t2.getTime()-t1.getTime())/1000;
-                var vitesse;
-                if (dt != 0) {
-                  vitesse = (dd / dt) * 1.94384;
-                }
-                else {
-                  vitesse = 0;
-                }
-                v.push(vitesse);
-                chartxy.push([d, vitesse]);
-                d = d + dd;
-              }
-          }
-          vmax = v.reduce((a, b) => { return Math.max(a, b) });
-          $("#seuil").val((vmax/2).toFixed(2));
-          var polyline = L.polyline(xy, {color: 'black'});
-          map.fitBounds(polyline.getBounds());
-        
-        ready();
-        
-        google.load('visualization', '1.0', {'packages':['corechart']});
-        google.setOnLoadCallback(drawChart);
-      
-      }
-  }
- )}
+    type: "GET",
+    url: url,
+    dataType: "xml",
+    success: function (xml) {
+      var i;
+      var t1, t2, dd, dt;
 
-litGPX(getParameterByName('url'), times, xy, v, dessine);
+      $(xml)
+        .find("trkpt")
+        .each(function () {
+          if ($(this).find("time").length == 1) {
+            time = $(this).find("time").text();
+            lat = parseFloat($(this).attr("lat"));
+            lon = parseFloat($(this).attr("lon"));
+
+            times.push(time);
+            var coord = [];
+            coord[0] = lat;
+            coord[1] = lon;
+            xy.push(coord);
+          }
+        });
+
+      var d = 0;
+      chartxy = [];
+      chartxy.push(["Temps", "Vitesse"]);
+      for (i = 0; i < times.length; i++) {
+        if (i == 0) {
+          v.push(0.0);
+        } else {
+          dd = calcCrow(xy[i][0], xy[i][1], xy[i - 1][0], xy[i - 1][1]) * 1000;
+          t1 = new Date(times[i - 1]);
+          t2 = new Date(times[i]);
+          dt = (t2.getTime() - t1.getTime()) / 1000;
+          var vitesse;
+          if (dt != 0) {
+            vitesse = (dd / dt) * 1.94384;
+          } else {
+            vitesse = 0;
+          }
+          v.push(vitesse);
+          chartxy.push([d, vitesse]);
+          d = d + dd;
+        }
+      }
+      vmax = v.reduce((a, b) => {
+        return Math.max(a, b);
+      });
+      $("#seuil").val((vmax / 2).toFixed(2));
+      var polyline = L.polyline(xy, { color: "black" });
+      map.fitBounds(polyline.getBounds());
+
+      ready();
+
+      google.load("visualization", "1.0", { packages: ["corechart"] });
+      google.setOnLoadCallback(drawChart);
+    },
+  });
+}
+
+litGPX(getParameterByName("url"), times, xy, v, dessine);
 
 function dessine() {
-  
-  polylignes= [];
+  polylignes = [];
   if (marker != null) {
     marker.remove();
   }
-  
+
   var seuil = $("#seuil").val();
 
   var xy2 = [];
@@ -108,132 +107,176 @@ function dessine() {
     xy2.push(xy[i]);
     if (v[i] > seuil) {
       cat = 1;
-    }
-    else {
+    } else {
       cat = 0;
     }
     if (cat0 != cat || xy2.length >= 100) {
       if (cat0 == 0) {
-        polylignes.push(L.polyline(xy2, {color: 'blue'}));
-      }
-      else {
-        polylignes.push(L.polyline(xy2, {color: 'red'}));
+        polylignes.push(L.polyline(xy2, { color: "blue" }));
+      } else {
+        polylignes.push(L.polyline(xy2, { color: "red" }));
       }
       xy2 = [];
       xy2.push(xy[i]);
       cat0 = cat;
     }
-  }      
+  }
   trace = L.layerGroup(polylignes).addTo(map);
-  
+
   marker = L.marker(xy[0]).addTo(map);
-}      
+}
 
-var map = L.map('map');
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  layers: [trace]
-  }).addTo(map);
+var map = L.map("map");
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  layers: [trace],
+}).addTo(map);
 
-  reportWindowSize();
+reportWindowSize();
 
-$("#seuil").change(function() {
-  $("#curseur").val(($("#seuil").val())/vmax*100);
-  drawAdditionalHAxis(chart, $("#seuil").val()); 
+$("#seuil").change(function () {
+  $("#curseur").val(($("#seuil").val() / vmax) * 100);
+  CreeLigneSeuil(chart, $("#seuil").val());
   map.removeLayer(trace);
   dessine();
 });
 
-$("#curseur").change(function() {
-  $("#seuil").val((($("#curseur").val())*vmax/100).toFixed(2));
-  drawAdditionalHAxis(chart, $("#seuil").val()); 
+$("#curseur").change(function () {
+  $("#seuil").val((($("#curseur").val() * vmax) / 100).toFixed(2));
+  CreeLigneSeuil(chart, $("#seuil").val());
   map.removeLayer(trace);
   dessine();
 });
 
 function reportWindowSize() {
-  document.getElementById("map").style.height = window.innerHeight-150-20 + 'px';
+  document.getElementById("map").style.height =
+    window.innerHeight - 150 - 20 + "px";
 }
 document.getElementsByTagName("body")[0].onresize = reportWindowSize;
 
 // ------------------------------------------------------------------------ chart
 
+var selectedElement = null;
+var currentY = 0;
+
 function drawChart() {
-  
   var data = google.visualization.arrayToDataTable(chartxy);
 
   var options = {
-    vAxis: {viewWindowMode: "explicit",   
-            viewWindow: {min: 0,max:vmax}
-           }, 
+    vAxis: { viewWindowMode: "explicit", viewWindow: { min: 0, max: vmax } },
     pointSize: 1,
-    legend: { position: "none" }
+    legend: { position: "none" },
   };
 
-chart = new google.visualization.LineChart(document.getElementById('chart'));     
+  chart = new google.visualization.LineChart(document.getElementById("chart"));
   chart.draw(data, options);
-  drawAdditionalHAxis(chart, $("#seuil").val()); 
-  
-  google.visualization.events.addListener(chart, 'onmouseover', function(e) {
-      marker.setLatLng(xy[e.row]);
-    });
 
-/*    
-  $("line").mousedown(function() {
-      down = true;
-    });  
+  CreeLigneSeuil(chart, $("#seuil").val());
 
-    $("line").mouseup(function() {
-      down = false;
-    });  
-    
-    $("line").mousemove(function(e) {
-      if (down) {
-        var layout = chart.getChartLayoutInterface();
-        var chartArea = layout.getChartAreaBoundingBox();
-        var H = layout.getChartAreaBoundingBox().height;
-        var Y = $("#chart").last().offset().top + H - e.clientY + 30;
-        var y = Y * vmax / H;
-        console.log(Y);
-        drawAdditionalHAxis(chart, y); 
-      }
-    });  
-*/
+  google.visualization.events.addListener(chart, "onmouseover", function (e) {
+    marker.setLatLng(xy[e.row]);
+  });
 }
 
-$(window).resize(function(){
+$(window).resize(function () {
   drawChart();
 });
 
-function drawAdditionalHAxis(chart, y){
+function CreeLigneSeuil(chart, y) {
   var layout = chart.getChartLayoutInterface();
   var chartArea = layout.getChartAreaBoundingBox();
-  var svg = chart.getContainer().getElementsByTagName("svg")[0];
   var yLoc = layout.getYLocation(y);
-  $("line").remove();
-  line = createLine(chartArea.left, yLoc, chartArea.width + chartArea.left, yLoc, "#FF0000", 2); 
-  svg.appendChild(line); 
+  $(".ligne").remove();
+  createLine(
+    chart,
+    chartArea.left,
+    yLoc,
+    chartArea.width + chartArea.left,
+    yLoc,
+    "#FF0000",
+    2
+  );
+
+  selectedElement = null;
+  currentY = 0;
+  registercb();
 }
 
-function createLine(x1, y1, x2, y2, color, w) {
+function createLine(chart, x1, y1, x2, y2, color, w) {
+  var svg = chart.getContainer().getElementsByTagName("svg")[0];
+
+  var svg2 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg2.setAttribute("class", "ligne");
+  svg2.setAttribute("x", x1);
+  svg2.setAttribute("y", y1);
+  svg2.setAttribute("width", x2 - x1);
+  svg2.setAttribute("height", 10);
+  svg.appendChild(svg2);
+
+  var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  rect.setAttribute("x", 0);
+  rect.setAttribute("y", 0);
+  rect.setAttribute("width", "100%");
+  rect.setAttribute("height", "100%");
+  rect.setAttribute("stroke", color);
+  rect.setAttribute("stroke-width", 1);
+  rect.setAttribute("fill-opacity", 0);
+  rect.setAttribute("stroke-opacity", 0);
+  svg2.appendChild(rect);
+
   var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line.setAttribute("x1", x1);
-  line.setAttribute("y1", y1);
-  line.setAttribute("x2", x2);
-  line.setAttribute("y2", y2);
+  line.setAttribute("x1", 0);
+  line.setAttribute("y1", 5);
+  line.setAttribute("x2", x2 - x1);
+  line.setAttribute("y2", 5);
   line.setAttribute("stroke", color);
   line.setAttribute("stroke-width", w);
-  return line;
+  svg2.appendChild(line);
 }
-                 
+
+var registercb = function () {
+  $(".ligne")
+    .mousedown(function (e) {
+      currentY = e.clientY;
+      selectedElement = e.target;
+    })
+    .mousemove(function (e) {
+      if (selectedElement) {
+        var y = ligneGety(e, e.clientY);
+        if (y >= 0 && y <= vmax) {
+          var dy =
+            parseInt($(this)[0].getAttribute("y")) + e.clientY - currentY;
+          $(this)[0].setAttribute("y", dy);
+          currentY = e.clientY;
+  
+          $("#seuil").val(y.toFixed(2));
+          $("#curseur").val(($("#seuil").val() / vmax) * 100);
+          map.removeLayer(trace);
+          dessine();
+        }
+      }
+    })
+    .mouseup(function (e) {
+      selectedElement = null;
+    });
+};
+
+function ligneGety(e, Y) {
+  var layout = chart.getChartLayoutInterface();
+  var H = layout.getChartAreaBoundingBox().height;
+  var Y = $("#chart").last().offset().top + H - e.clientY + 30;
+  return (y = (Y * vmax) / H);
+}
+
 // ----------------------------------------
 
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}  
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
--- sensations/visu-gpx/visu-gpx.php
+++ sensations/visu-gpx/visu-gpx.php
@@ -1,43 +1,44 @@
-
 <!DOCTYPE html>
 <html lang="fr">
-   <head>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
-   <style>
-		#map {
-			width: 100%;
-			height: 800px;
-		}
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+    <style>
+    #map {
+        width: 100%;
+        height: 800px;
+    }
 
     #control {
-      height: 150px;
+        height: 150px;
     }
 
     #seuil {
-      width: 40px;
+        width: 40px;
     }
+
     .slider-wrapper {
-      display: inline-block;
-      width: 20px;
-      padding: 5px;
+        display: inline-block;
+        width: 20px;
+        padding: 5px;
     }
 
     .slider-wrapper input {
-      width: 140px;
-      height: 20px;
-      margin: 0;
-      transform-origin: 70px 70px;
-      transform: rotate(-90deg);
+        width: 140px;
+        height: 20px;
+        margin: 0;
+        transform-origin: 70px 70px;
+        transform: rotate(-90deg);
     }
 
     .seuil {
-      display: inline-block;
-      width: 130px;
+        display: inline-block;
+        width: 130px;
     }
 
     #reglages {
-      float: left;
-      height: 100%;
+        float: left;
+        height: 100%;
     }
 
     #courbe {
@@ -49,39 +50,39 @@
     }
 
     #chart {
-      width: 100%;
-      height: 100%;
+        width: 100%;
+        height: 100%;
     }
-/*
-    line {
-      cursor: row-resize;
+
+    .ligne {
+        cursor: row-resize;
     }
- */   
-   </style>
+    </style>
 
-   </head>
+</head>
 
-   <body> 
-        <div id="map"></div>
-        <div id="control">
-          <div id="reglages">
-            <div class="slider-wrapper">    
-              <input type="range" id="curseur" min="0" max="100" step="any">
+<body>
+    <div id="map"></div>
+    <div id="control">
+        <div id="reglages">
+            <div class="slider-wrapper">
+                <input type="range" id="curseur" min="0" max="100" step="any">
             </div>
             <div class="seuil">
-              <label for="name">Seuil (kts)</label>
-              <input type="text" id="seuil"></input> 
+                <label for="name">Seuil (kts)</label>
+                <input type="text" id="seuil"></input>
             </div>
-          </div>
-          <div id="courbe">
+        </div>
+        <div id="courbe">
             <div id="chart"></div>
-          </div>
-      </div> 
+        </div>
+    </div>
 
-        <script src="/js/jquery.min.js"></script>  
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
-        <script src="https://www.google.com/jsapi"></script>
-        <script src="trace.js"></script>
- 
-    </body>
-</html>   
+    <script src="/js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
+    <script src="https://www.google.com/jsapi"></script>
+    <script src="trace.js"></script>
+
+</body>
+
+</html>
