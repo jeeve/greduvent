66bbdb0a92961735ef61e4a795d371aacacf5e07
jeeve <jyvaisj@gmail.com>
2021-09-06 13:16:09 +0200

    a propos

--- a-propos/js/favoris.js
+++ a-propos/js/favoris.js
@@ -1,6 +1,6 @@
 function getFavoris() {
 		$.ajax({
-			url: "https://spreadsheets.google.com/feeds/list/166nDewURI08ookiFuut1lrI_47TXvngWEj7_-YKFstw/od6/public/values?alt=json",
+			url: "https://sheets.googleapis.com/v4/spreadsheets/166nDewURI08ookiFuut1lrI_47TXvngWEj7_-YKFstw/values/Favoris?key=" + sheetapi, // "https://spreadsheets.google.com/feeds/list/166nDewURI08ookiFuut1lrI_47TXvngWEj7_-YKFstw/od6/public/values?alt=json",
 			type: 'GET',
 			crossDomain: true,
 			dataType: 'json'
@@ -14,12 +14,12 @@ function getFavoris() {
 				htmlRomans = "<table>";
 				htmlFilms = "<table>";
 				htmlLogiciels = "<table>";
-				for (i=0; i < data.feed.entry.length; i++) {
+				for (i=1; i < data.values.length; i++) {
 					ligneHtml = "<tr>";
-					ligne = data.feed.entry[i];
-					nom = ligne.title.$t;
-					auteur = ligne.gsx$auteur.$t;
-					type = ligne.gsx$type.$t;
+					ligne = data.values[i];
+					nom = ligne[0];
+					auteur = ligne[1];
+					type = ligne[2];
 					if (nom.substring(0, 4) == 'http') {
 						ligneHtml = ligneHtml + '<td><a href="' + nom + '" target="_blank">' + nom + '</td>';
 					}
