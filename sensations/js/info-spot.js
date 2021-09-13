19fc54450214b213e4a778d2658d1abe1660a9a3
jeeve <jyvaisj@gmail.com>
2021-09-13 16:18:31 +0200

    info-spots

--- sensations/js/info-spot.js
+++ sensations/js/info-spot.js
@@ -1,24 +1,24 @@
 function getInfoSpot() {
 	$.ajax({
-		url: "https://spreadsheets.google.com/feeds/list/1O3WDo7864s7npqM7r7zd2XXVWeVqhuqjgmzfObh40gY/default/public/values?alt=json",
+		url: "https://sheets.googleapis.com/v4/spreadsheets/1O3WDo7864s7npqM7r7zd2XXVWeVqhuqjgmzfObh40gY/values/Spots?key=" + sheetapi, // "https://spreadsheets.google.com/feeds/list/1O3WDo7864s7npqM7r7zd2XXVWeVqhuqjgmzfObh40gY/default/public/values?alt=json",
 		type: 'GET',
 		crossDomain: true,
 		dataType: 'json'
 	}).then(function(data) {
 			var ligne, iPoses, iVairessurmarne, iJablines, iMoisson, iGrandeparoisse, iSaintQuentin, iEcluzelles, iForetOrient;
 			var col1;
-			for (i=data.feed.entry.length-1; i >= 0 ; i--) {
-				ligne = data.feed.entry[i];
-				col1 = ligne.title.$t;
+			for (i=data.values.length-1; i >= 1 ; i--) {
+				ligne = data.values[i];
+				col1 = ligne[0];
 				if (col1 == 'Info') {
-					iPoses = ligne.gsx$poses.$t;
-					iVairessurmarne = ligne.gsx$vairessurmarne.$t;
-					iJablines = ligne.gsx$jablines.$t;
-					iMoisson = ligne.gsx$moisson.$t;
-					iGrandeparoisse = ligne.gsx$grandeparoisse.$t;
-					iSaintQuentin = ligne.gsx$saintquentinenyvelines.$t;
-					iEcluzelles = ligne.gsx$ecluzelles.$t;
-					iForetOrient = ligne.gsx$foretorient.$t;
+					iPoses = ligne[1];
+					iVairessurmarne = ligne[2];
+					iJablines = ligne[3];
+					iMoisson = ligne[4];
+					iGrandeparoisse = ligne[5];
+					iSaintQuentin = ligne[6];
+					iEcluzelles = ligne[7];
+					iForetOrient = ligne[8];
 												
 					$('.infoPoses').html(iPoses);
 					$('.infoVairesSurMarne').html(iVairessurmarne);
