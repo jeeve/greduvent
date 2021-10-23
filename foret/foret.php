6d85616b89834ed57c0591e3f0556ba63de1928a
jeeve <jyvaisj@gmail.com>
2021-10-23 19:49:40 +0200

    ""

--- foret/foret.php
+++ foret/foret.php
@@ -177,7 +177,7 @@
                     <div class="row">
                         <div class="col-xs-12 col-sm-1 fond"></div>
                         <div class="col-xs-12 col-sm-10 fond">
-                            <h2>Randonnée "<a href="https://www.visorando.com/randonnee-le-sentier-des-belvederes/" target="_blank">Le Sentier des Belvédères</a>"" - 23 octobre 2021 - <a
+                            <h2>Randonnée "<a href="https://www.visorando.com/randonnee-le-sentier-des-belvederes/" target="_blank">Le Sentier des Belvédères</a>" - 23 octobre 2021 - <a
                                     href="https://photos.app.goo.gl/yP4TVzXuq8avWLuV9" target="_blank">photos</a></h2>
                             <div class="embed-responsive embed-responsive-4by3 ombre-image">
 
