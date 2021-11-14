eecdf465a6d784f719d6704076dae542a3c1b73b
jeeve <jyvaisj@gmail.com>
2021-11-15 00:05:11 +0100

    glisse

--- sensations/sensations.php
+++ sensations/sensations.php
@@ -98,7 +98,7 @@
                     <div class="row">
 
                         <div class="col-xs-12 fond">
-                            <a href="windsurf-kitesurf.php">
+                            <a href="glisse.php">
                                 <p align="center">
                                     <img alt="Baie de l'Arguenon - 15 juillet 2007 - photo Mireille M."
                                         title="Baie de l'Arguenon - 15 juillet 2007 - photo Mireille M."
@@ -115,7 +115,7 @@
                         <div class="col-xs-12 col-sm-1 fond"></div>
                         <div class="col-xs-12 col-sm-10 fond">
                             <p align="center">
-                                <a href="windsurf-kitesurf.php">
+                                <a href="glisse.php">
                                     <img alt="Kitesurf à Saint-Jacut de la Mer - 24 juillet 2007 - photo André J."
                                         title="Kitesurf à Saint-Jacut de la Mer - 24 juillet 2007 - photo André J."
                                         src="images/kitesurf-saint-jacut.jpg" class="img-responsive ombre-image" />
