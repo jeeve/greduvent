17a70948bd7905e43ee3a49846ea7f12278ffa12
jeeve <jyvaisj@gmail.com>
2022-03-09 14:49:53 +0100

    ordre

---
+++ cv/includes/header.php
@@ -0,0 +1,16 @@
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <META NAME="Keywords" CONTENT="CV"/>
+      <META NAME="Author" CONTENT="Jean-Valéry JULIEN"/>
+      <link rel="shortcut icon" href="/images/favicon_16_16.ico" />
+      <link rel="icon" href="/images/favicon_32_32.png" sizes="32x32" />
+      <link rel="icon" href="/images/favicon_48_48.png" sizes="48x48" /><link rel="apple-touch-icon" href="/images/favicon_114_114.png" />
+  	  <link rel="alternate" type="application/rss+xml" title="Flux RSS" href="/news.xml" />
+      <link href="/css/bootstrap.min.css" rel="stylesheet">
+      <link href="/css/bootply.css" rel="stylesheet">
+      <link href="/css/style.css" rel="stylesheet">
+      <link href="/css/fond.css" rel="stylesheet">
+
+
+
