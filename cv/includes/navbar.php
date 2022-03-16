b7de5793453331b5700ae6e8f68f7bab70086b59
jeeve <jyvaisj@gmail.com>
2022-03-16 15:00:18 +0100

    maj

--- cv/includes/navbar.php
+++ cv/includes/navbar.php
@@ -1,12 +1,12 @@
       <div class="navbar navbar-default navbar-fixed-top" role="navigation">
          <div class="container">
-            <div class="navbar-header">
-               <button id="bouton-bas-page" type="button" class="navbar-toggle hidden" style="padding-top:5px;" data-toggle="bas-page" data-target=".sidebar-nav">     
-                  <span class="glyphicon glyphicon-arrow-down" style="color: grey; padding-top: 3px;"></span>
-               </button>			
+            <div class="navbar-header">		
                <button id="bouton-menu" type="button" class="navbar-toggle hidden" style="padding-bottom:5px; padding-top:5px" data-toggle="offcanvas" data-target=".sidebar-nav">    
                   <span class="glyphicon glyphicon-arrow-up" style="color: grey; padding-top: 3px;"></span>
                </button>
+               <button id="bouton-bas-page" type="button" class="navbar-toggle hidden" style="padding-top:5px;" data-toggle="bas-page" data-target=".sidebar-nav">     
+                  <span class="glyphicon glyphicon-arrow-down" style="color: grey; padding-top: 3px;"></span>
+               </button>	
                <a class="navbar-brand navbar-brand-texte hidden-xs" href="./#home">Jean-Valéry JULIEN</a>
                <a class="navbar-brand navbar-brand-texte visible-xs" href="./#home">JV JULIEN</a>
                <a class="navbar-brand navbar-brand-texte" href="./#experience">Expérience</a>
