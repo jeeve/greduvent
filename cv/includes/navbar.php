6b0467a40662d1299ed369f7d4ba00d88c64c689
jeeve <jyvaisj@gmail.com>
2022-03-16 11:42:36 +0100

    size

--- cv/includes/navbar.php
+++ cv/includes/navbar.php
@@ -7,7 +7,8 @@
                <button id="bouton-menu" type="button" class="navbar-toggle hidden" style="padding-bottom:5px; padding-top:5px" data-toggle="offcanvas" data-target=".sidebar-nav">    
                   <span class="glyphicon glyphicon-arrow-up" style="color: grey; padding-top: 3px;"></span>
                </button>
-               <a class="navbar-brand navbar-brand-texte" href="./#home">Jean-Valéry JULIEN</a>
+               <a class="navbar-brand navbar-brand-texte hidden-xs" href="./#home">Jean-Valéry JULIEN</a>
+               <a class="navbar-brand navbar-brand-texte visible-xs" href="./#home">JV JULIEN</a>
                <a class="navbar-brand navbar-brand-texte" href="./#experience">Expérience</a>
                <a class="navbar-brand navbar-brand-texte" href="./#formation">Formation</a>
                <a class="navbar-brand navbar-brand-texte" href="./#divers">à propos</a>
