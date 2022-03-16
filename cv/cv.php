29c0d2d7c3337316a566f81ef63c5e3ecb064082
jeeve <jyvaisj@gmail.com>
2022-03-16 22:31:39 +0100

    maj

--- cv/cv.php
+++ cv/cv.php
@@ -33,7 +33,7 @@
                             <h3><strong>Développeur informatique</strong></h3>
                             <p>Date de naissance : 1972 (49 ans)</p>
                             <p>Gentilly (94)</p>
-                            <p><a class="mail" href="mailto:jeanvalery.julien@gmail.com">jeanvalery.julien@gmail.com</a>
+                            <p><a target="-blank" class="mail" href="https://docs.google.com/forms/d/e/1FAIpQLSeqrcDEWlgXWPVxr-wBuH7s0XvJSQLcMwyXES9m16BSrsYU-g/viewform?usp=pp_url">contact</a>
                             </p>
 
                             <br>
--- cv/includes/footer.php
+++ cv/includes/footer.php
@@ -1,7 +1,7 @@
 	  <script src="/js/jquery.min.js"></script>
       <script src="/js/bootstrap.min.js"></script>
       <script src="/js/bootply.js"></script>
-	  <script src="/js/script.js"></script>
+	  <script src="./js/script.js"></script>
 	  <script src="/js/navbar.js"></script>	  
 	  <script src="/js/jquery.mobile.touch.min.js"></script>
 	  <script>
