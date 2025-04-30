668e1b57a533682179366277ca832f0c6b4a82eb
jvjulien <jean-valery.julien@cetim.fr>
2025-04-30 15:38:30 +0200

    maj

--- sensations/mapbox/index.php
+++ sensations/mapbox/index.php
@@ -53,7 +53,7 @@
 
 <script>
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiamVldmUiLCJhIjoiY21hM2piNWppMWx6dzJrc2g0eGRybXVwMSJ9.Mb9SiR-LznRZl789do68AQ'; 
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVldmUiLCJhIjoiY21hM3hoM2prMDB5bjJvczh2N2p6ZDVwbSJ9.cXhPhElcIwOcCtIxLDJpRw'; 
 
 const map = new mapboxgl.Map({
     container: 'map',
