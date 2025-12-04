9882a9bcea9c999478abbe0fc84338a17ae2f1af
jeeve <jyvaisj@gmail.com>
2025-12-04 19:36:56 +0100

    lien cuisine spectrale

--- includes/audio-player.php
+++ includes/audio-player.php
@@ -41,7 +41,7 @@ if (is_dir($musicDir)) {
     <div id="side-player-panel">
         <div class="app-container-side">
             <header class="side-header">
-                <h3>Sonic<span class="highlight">Flow</span></h3>
+                <h3><a href="https://www.youtube.com/playlist?list=PLvp4urJ2GQfiRa0B5ZYjLukDXRb4fA4zr" target="_blank" style="text-decoration: none; color: #fff;">Cuisine Spectrale</a></h3>
                 <label for="file-input" class="upload-btn-side" title="Ajouter des fichiers">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
                 </label>
